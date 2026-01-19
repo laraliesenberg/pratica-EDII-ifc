2a13cd1c8313cc6819dd3db41bbc3ee2c6eed3b2
Lara Liesenberg <laraliesenberg@gmail.com>
2026-01-19 17:18:17 -0300

    Update aresta.py

--- ATV5-GRAFO/aresta.py
+++ ATV5-GRAFO/aresta.py
@@ -1,1 +1,12 @@
+class Aresta:
+    def __init__(self, id_aresta, u, v, peso=1):
+        self.id = id_aresta
+        self.vertices = [u, v]
+        self.peso = peso
 
+        u.add_aresta(self)
+        v.add_aresta(self)
+
+    def imprimeAresta(self):
+        return f"Aresta: {self.vertices[0].id}, {self.vertices[1].id}"
+    
