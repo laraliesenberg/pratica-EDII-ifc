8fffb4881740c48d27d42c0a09cd167580c6feae
Lara Liesenberg <laraliesenberg@gmail.com>
2026-01-19 17:14:32 -0300

    Add Aresta class for graph edges

--- ATV2-GRAFO/aresta.py
+++ ATV2-GRAFO/aresta.py
@@ -1,1 +1,10 @@
+class Aresta:
+    def __init__(self, id_aresta, u, v):
+        self.id = id_aresta
+        self.vertices = [u, v]
 
+        u.add_aresta(self)
+        v.add_aresta(self)
+
+    def imprimeAresta(self):
+        return f"Aresta: {self.vertices[0].id}, {self.vertices[1].id}"
